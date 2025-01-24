ab43cf7ce857856c2c78db402c1c465b35401213
xcode <xcode@xcode.com>
2025-01-25 02:10:05 +0530

    Updating requirements.txt

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,17 +6,17 @@ import logging
 import os
 import dagshub
 
-mlflow.set_tracking_uri('https://dagshub.com/k.ritesh2887/mlops-mini-project.mlflow')
-dagshub.init(repo_owner='k.ritesh2887', repo_name='mlops-mini-project', mlflow=True)
+#mlflow.set_tracking_uri('https://dagshub.com/k.ritesh2887/mlops-mini-project.mlflow')
+#dagshub.init(repo_owner='k.ritesh2887', repo_name='mlops-mini-project', mlflow=True)
 
 
 # Set up DagsHub credentials for MLflow tracking
-#dagshub_token = os.getenv("DAGSHUB_PAT")
-#if not dagshub_token:
-    #raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-#os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-#os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
 repo_owner = "k.ritesh2887"
